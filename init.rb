a4355a54da182b2bdf2e6bbc48f03ac6bc9194e1
Максим Петров <maksim.petrov@kodep.ru>
2015-08-13 16:14:42 +0300

    update

--- init.rb
+++ init.rb
@@ -3,7 +3,11 @@ Redmine::Plugin.register :redmine_achiever do
   author 'Maksim Petrov'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
-  settings :default => {'empty' => true}, :partial => 'settings/poll_settings'
+  url 'https://github.com/petrovmp89/redmine-achiver'
+  author_url ''
+  settings :default => {'achiever_url' => "http://achiever.kodep.ru:3000"}, :partial => 'settings/redmine_achiever_settings'
+end
+
+ActionDispatch::Callbacks.to_prepare do 
+  TimeEntry.send(:include, Patches::TimeEntryPatch)
 end
